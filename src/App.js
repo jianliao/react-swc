193ad71f8db082a90d246cdbb63d905943fe1363
JIAN LIAO <jianliao@JIANs-MacBook-Pro-2.local>
2022-11-22 13:20:13 -0800

    Test @swc-react/* canary release

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Theme } from '@swc-react/theme';
+import { Button } from '@swc-react/button';
+import { Card } from '@swc-react/card';
+import { Radio, RadioGroup } from '@swc-react/radio';
+import { ActionGroup } from '@swc-react/action-group';
+import { ActionButton } from '@swc-react/action-button';
+import { ProgressCircle } from '@swc-react/progress-circle';
+import { IconEdit } from '@swc-react/icons-workflow/Edit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Theme theme="spectrum" scale="medium" color="light">
+      <div className="App">
+        <Button click={() => alert('Hello')}>
+          <IconEdit slot="icon"></IconEdit>
+          Be right back!
+        </Button>
+        <Card heading="Card Heading" subheading="JPG Photo">
+          <img
+            slot="cover-photo"
+            src="https://picsum.photos/200/300"
+            alt='demo'
+          />
+          <div slot="footer">Footer</div>
+        </Card>
+        <RadioGroup selected="first" name="example">
+          <Radio value="first">Option 1</Radio>
+          <Radio value="second">Option 2</Radio>
+          <Radio value="third">Option 3</Radio>
+          <Radio value="fourth">Option 4</Radio>
+        </RadioGroup>
+        <ActionGroup selects="single" selected={["first"]}>
+          <ActionButton value="first">First</ActionButton>
+          <ActionButton value="second">Second</ActionButton>
+        </ActionGroup>
+        <ProgressCircle size='l' indeterminate />
+      </div>
+    </Theme>
   );
 }
 
